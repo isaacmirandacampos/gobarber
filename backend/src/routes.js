1036a7b3982988d57761a40e5feafab35f0e3810
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-21 19:57:38 -0300

    criando avatar e listando provider

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
+
 routes.put('/users', UserController.update);
 
 export default routes;
