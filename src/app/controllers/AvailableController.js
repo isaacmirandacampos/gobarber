3489cf9456b59d0d457ad3a469ddbd75341d9495
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-12-19 22:13:46 -0300

    finalized application

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -45,7 +45,7 @@ class AvailableController {
       '19:00',
     ];
 
-    const avaiable = schedule.map(time => {
+    const available = schedule.map(time => {
       const [hour, minute] = time.split(':');
       const value = setSeconds(
         setMinutes(setHours(searchDate, hour), minute),
@@ -55,13 +55,13 @@ class AvailableController {
       return {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-        avaiable:
+        available:
           isAfter(value, new Date()) &&
           !appointments.find(a => format(a.date, 'HH:mm') === time),
       };
     });
 
-    return res.json(avaiable);
+    return res.json(available);
   }
 }
 export default new AvailableController();
